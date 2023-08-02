c7501880869c6ed7b8fb8731216c9d8b435f1ebe
hugo B <62248962+0BAB1@users.noreply.github.com>
2023-08-02 17:36:22 +0200

    save of the day and stop ignoring my todo list

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ with open(filename, "r") as file:
     for line in file:
         #get data from the line to insert into a csv file
         data = lathe.interpret(line)
+
+print(lathe.globalTime/60 , " minutes")
